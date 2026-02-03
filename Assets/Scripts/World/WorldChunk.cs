cec8003f180f961efe01dbfb52cdb066d597c87d
Karcag Tamas <karcagtamas@outlook.com>
2026-02-03 16:11:10 +0100

    Portrait mode and infinite chunkscrolling

--- Assets/Scripts/World/WorldChunk.cs
+++ Assets/Scripts/World/WorldChunk.cs
@@ -1,15 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WorldChunk : MonoBehaviour
 {
-    [SerializeField] private float chunkHeight = 10f;
-    [SerializeField] private float recycleY = -12f;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float recycleY = -6f;
 
     void Update()
     {
+        transform.position += speed * Time.deltaTime * Vector3.down;
+
         if (transform.position.y < recycleY)
         {
-            transform.position += Vector3.up * chunkHeight * 3f;
+            var height = transform.localScale.y;
+            transform.position += 3f * height * Vector3.up;
         }
     }
 }
